696d460c5ca6412d044fb887599378617692b586
Christian Stussak <christian.stussak@imaginary.org>
2026-01-16 19:31:20 +0100

    Patch @tockawa/nfc-pcsc to properly export TypeScript types

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,6 +1,4 @@
 import { strict as assert } from "node:assert";
-
-// TODO: Patch @pokusew/nfc-pcsc according to https://hirok.io/posts/package-json-exports#targeting-node-js-esm-cjs-and-type-script
 import PCSC from "@tockawa/nfc-pcsc";
 
 import type { PreloadApi } from "./types";
