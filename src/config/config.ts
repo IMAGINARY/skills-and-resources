058c932539092ac56a48ce0d88b55278f563f572
Christian Stussak <christian.stussak@imaginary.org>
2026-02-11 15:25:29 +0100

    Add basic config validation via TypeBox
    This does not contain any details error handling for now.
    It just throws a `ParseError` if validation fails.

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,20 @@
 import type { DeepReadonly } from "vue";
 import app from "@/config/app.yaml";
 import content from "@/config/content.yaml";
+import { AppConfigSchema, ContentConfigSchema } from "@/types/config.ts";
+import { Value } from "typebox/value";
 
-import type { AppConfig, ContentConfig, Config } from "@/types/config";
+import type { Config } from "@/types/config";
 
 export async function loadConfig(): Promise<DeepReadonly<Config>> {
-  // TODO: Load config from file(s) at runtime
-  return structuredClone({
-    app: app as AppConfig,
-    content: content as ContentConfig,
-  } as Config);
+  try {
+    const config: Config = {
+      app: Value.Parse(AppConfigSchema, app),
+      content: Value.Parse(ContentConfigSchema, content),
+    };
+    return structuredClone(config);
+  } catch (parseError) {
+    console.log("Failed to parse configuration", parseError);
+    throw parseError;
+  }
 }
