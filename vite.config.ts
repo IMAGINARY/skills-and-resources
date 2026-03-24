d94f9296376cf7ed43072271a819f9f64512d7b8
Christian Stussak <christian.stussak@imaginary.org>
2026-03-24 12:21:01 +0100

    Add `GIT_COMMIT_HASH` via `process.env`

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
-import { resolve } from "path";
+import { execSync } from "node:child_process";
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import svgLoader from "vite-svg-loader";
 
+const gitCommitHash =
+  process.env.GIT_COMMIT_HASH || execSync("git rev-parse HEAD").toString().trim();
+
 export default defineConfig({
   base: "./",
+  define: {
+    "process.env.GIT_COMMIT_HASH": JSON.stringify(gitCommitHash),
+  },
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
